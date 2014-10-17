072e94695fad0556ab5632c4f889c9005ba2c15a
Kant <kant@wegdam.ewi.utwente.nl>
2014-10-18 01:47:38 +0200

    Slight changes to connect four case.

--- connectfour/separate/separate_7x6_ltsmin_sym.sh
+++ connectfour/separate/separate_7x6_ltsmin_sym.sh
@@ -4,4 +4,4 @@ BIN_DIR=../../bin/
 input=four7
 
 #${BIN_DIR}lps2lts-sym --mcrl2=-rjitty ${input}.lps
-${BIN_DIR}memtime ${BIN_DIR}lps2lts-sym --mcrl2=-rjitty -rgs ${input}.lps --vset=lddmc --order=par-prev
+${BIN_DIR}memtime ${BIN_DIR}lps2lts-sym --mcrl2=-rjitty -rgs ${input}.lps --vset=lddmc --order=par-prev --lddmc-tablesize=27
